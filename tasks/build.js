78c8ad67ef6eb25db88347cc0317567b18f7e884
Chris Basham <csbasham@iu.edu>
2014-12-19 14:13:22 -0500

    Render colorguard stats to HTML.

---
+++ tasks/build.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var fs = require('fs');
+var colorguard = require('colorguard');
+var rename = require('gulp-rename');
+var jade = require('gulp-jade');
+
+var locals = {};
+
+gulp.task('build', ['clean'], function() {
+  var css = fs.readFileSync('./src/styles/student.css', 'utf8');
+  var output = colorguard.inspect(css);
+  locals.pageTitle = 'student.css';
+  locals.audit = output;
+
+  return gulp.src('./src/*/*.jade')
+    .pipe(rename('index'))
+    .pipe(jade({
+      locals: locals,
+      pretty: true
+    }))
+    .pipe(gulp.dest('./build/'));
+});
